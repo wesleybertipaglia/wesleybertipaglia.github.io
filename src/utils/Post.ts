6915bf866b06ecb3d67a19c02a7b997acf6cd045
wesleybertipaglia <wesleybertipaglia@gmail.com>
2023-12-18 20:43:39 -0300

    feat(posts): add functions to get posts

--- src/utils/Post.ts
+++ src/utils/Post.ts
@@ -1,5 +1,14 @@
+import { getCollection } from "astro:content";
 export const enum PostView {
   compact,
   normal,
   full,
 }
+
+export const Projects = (await getCollection("projects")).sort(
+  (a, b) => a.data.pubDate.valueOf() - b.data.pubDate.valueOf()
+);
+
+export const Posts = (await getCollection("blog")).sort(
+  (a, b) => a.data.pubDate.valueOf() - b.data.pubDate.valueOf()
+);
