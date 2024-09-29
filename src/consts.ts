209d818314fdb9a674f4586fc78ffca0ea399e71
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-09-29 16:05:20 -0300

    feat: update consts and types

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,49 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+import type { Site, Metadata, Link } from "@types"
 
-export const SITE_TITLE = "Wesley Bertipaglia";
-export const SITE_SEPARATOR = " · ";
-export const SITE_DESCRIPTION = "Developer & Tech Enthusiast";
+export const SITE: Site = {
+    title: "Wesley Bertipaglia",
+    description: "Software engineer from Brazil, focused on web development.",
+    author: "Wesley Bertipaglia",
+    keywords: "Software, Engineer, Architect, Developer, Programmer, Coder, Founder"
+}
+
+export const HOME: Metadata = {
+    title: "Home",
+    description: "Software engineer from Brazil, focused on web development.",
+}
+
+export const BLOG: Metadata = {
+    title: "Blog",
+    description: "A collection of articles on topics I am passionate about.",
+}
+
+export const EXPERIENCE: Metadata = {
+    title: "Experience",
+    description: "Where I have worked and what I have done.",
+}
+
+export const PROJECTS: Metadata = {
+    title: "Projects",
+    description: "A collection of my projects, with links to repositories and demos.",
+}
+
+export const SOCIALS: Link[] = [
+    {
+        title: "github",
+        url: "https://github.com/wesleybertipaglia",
+        icon: "github",
+        main: true,
+    },
+    {
+        title: "linkedin",
+        url: "https://www.linkedin.com/in/wesleybertipaglia",
+        icon: "linkedin",
+        main: true,
+    },
+    {
+        title: "threads",
+        url: "https://www.threads.net/@wesleyberti_",
+        icon: "threads",
+        main: false,
+    },
+];
---
+++ src/types.ts
@@ -0,0 +1,18 @@
+export type Site = {
+  title: string
+  description: string
+  author: string
+  keywords: string
+}
+
+export type Metadata = {
+  title: string
+  description: string
+}
+
+export type Link = {
+  title: string
+  url: string
+  icon?: string
+  main: boolean
+}
