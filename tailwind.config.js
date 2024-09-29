8ce9da9157fd45b4d2f35383a86328bad02b5156
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-09-29 15:59:29 -0300

    feat: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,57 +1,49 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		"./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,css,scss,sass,postcss}",
+		"./src/styles/**/*.{css, scss, sass, postcss}",
+	],
+	darkMode: 'selector',
 	theme: {
 		extend: {
 			colors: {
-				'primary': 'var(--primary)',
-				'body': {
-					1: 'var(--body-1)',
-					2: 'var(--body-2)',
-					3: 'var(--body-3)',
+				light: {
+					background: {
+						1: '#f6f6f6',
+						2: '#e6e6e6',
+						3: '#d6d6d6',
+					},
+					text: {
+						1: '#161616',
+						2: '#262626',
+						3: '#363636',
+					},
+					border: {
+						1: '#464646',
+						2: '#565656',
+						3: '#666666',
+					},
 				},
-				'text': {
-					1: 'var(--text-1)',
-					2: 'var(--text-2)',
-					3: 'var(--text-3)',
+				dark: {
+					background: {
+						1: '#161616',
+						2: '#262626',
+						3: '#363636',
+					},
+					text: {
+						1: '#f6f6f6',
+						2: '#e6e6e6',
+						3: '#d6d6d6',
+					},
+					border: {
+						1: '#c6c6c6',
+						2: '#b6b6b6',
+						3: '#a6a6a6',
+					},
 				},
-				'accent': 'var(--accent)'
-			}
-		},
-		fontFamily: {
-			sans: ['Inter var', 'Inter', 'sans-serif'],
-		},
-		animation: {
-			'fade-in': 'fadeIn 0.5s ease-in-out',
-			'fade-in-slow': 'fadeIn 1s ease-in-out',
-			'marquee': 'marquee 20s linear infinite',
-			'hang': 'hang 1s ease-in-out',
-			'hang-slow': 'hang 2s ease-in-out',
-			'hand-wave': 'handWave 1s ease-in-out infinite',
-		},
-		keyframes: {
-			fadeIn: {
-				'0%': { opacity: 0 },
-				'100%': { opacity: 1 },
-			},
-			marquee: {
-				'0%': { transform: 'translateX(0)' },
-				'100%': { transform: 'translateX(calc(-100% - 4rem))' },
-			},
-			hang: {
-				'0%': { transform: 'translateY(0)' },
-				'100%': { transform: 'translateY(-10px)' },
-			},
-			handWave: {
-				'0%': { transform: 'rotate(0deg)' },
-				'50%': { transform: 'rotate(30deg)' },
-				'100%': { transform: 'rotate(0deg)' },
 			},
 		},
-	},
-	variants: {
-		extend: {},
 	},
 	plugins: [],
-	darkMode: 'selector'
 }
