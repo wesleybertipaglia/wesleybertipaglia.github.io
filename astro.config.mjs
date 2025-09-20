d133120fcaff0060a1fc1d7b1e1c0ced0efdfde8
wesleybertipaglia <wesleybertipaglia@gmail.com>
2025-09-19 23:24:00 -0300

    feat: update packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-
 import icon from 'astro-icon';
 
 export default defineConfig({
