b3ff634deae274e678609e26107e5dbff02d2f30
wesleybertipaglia <wesleybertipaglia@gmail.com>
2025-10-24 10:34:10 -0300

    style: standardize indentation in article styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 
 export default defineConfig({
