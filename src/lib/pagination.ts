0d458f49a003c94ae1990962bde5499c9ec03739
wesleybertipaglia <wesleybertipaglia@gmail.com>
2025-10-23 22:01:30 -0300

    feat: implement pagination for blog, projects, series, and work pages

---
+++ src/lib/pagination.ts
@@ -0,0 +1,20 @@
+interface PaginationProps {
+    url: URL;
+}
+
+export interface Pagination {
+    page: number;
+    size: number;
+    offset: number;
+}
+
+export const getPagination = ({ url }: PaginationProps): Pagination => {
+    const page = Math.max(1, parseInt(url.searchParams.get('page') || '1'));
+    const size = Math.max(1, parseInt(url.searchParams.get('size') || '10'));
+
+    return {
+        page,
+        size,
+        offset: (page - 1) * size,
+    };
+};
